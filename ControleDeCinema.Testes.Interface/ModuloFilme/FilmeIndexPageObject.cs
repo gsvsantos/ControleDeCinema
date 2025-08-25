2db3f7d83453f0ec20b283f47d92d9ab9a530280
gsvsantos <gustavocnsantos02@gmail.com>
2025-08-25 00:11:09 -0300

    feat: Implementa estrutura de teste de interface, com teste inicial de cadastro do ModuloFilme.

---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeIndexPageObject.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloFilme;
+
+public class FilmeIndexPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public FilmeIndexPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+    }
+
+    public FilmeIndexPageObject IrPara(string enderecoBase)
+    {
+        driver.Navigate().GoToUrl($"{enderecoBase.TrimEnd('/')}/filmes");
+
+        wait.Until(d => d.Url.Contains("/filmes", StringComparison.OrdinalIgnoreCase));
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Displayed);
+
+        return this;
+    }
+
+    public FilmeFormPageObject ClickCadastrar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']"))).Click();
+
+        return new(driver);
+    }
+
+    public FilmeFormPageObject ClickEditar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnEditar']"))).Click();
+
+        return new(driver);
+    }
+
+    public FilmeFormPageObject ClickExcluir()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnExcluir']"))).Click();
+
+        return new(driver);
+    }
+
+    public bool ContemFilme(string titulo)
+    {
+        return driver.PageSource.Contains(titulo);
+    }
+}
---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeInterfaceTestes.cs
@@ -0,0 +1,50 @@
+using ControleDeCinema.Testes.Interface.Compartilhado;
+using ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+namespace ControleDeCinema.Testes.Interface.ModuloFilme;
+
+[TestClass]
+[TestCategory("Testes de Interface de Filme")]
+public sealed class FilmeInterfaceTestes : TestFixture
+{
+    [TestInitialize]
+    public override void InicializarTeste()
+    {
+        base.InicializarTeste();
+
+        driver.Manage().Cookies.DeleteAllCookies();
+
+        RegistrarContaEmpresarial();
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Filme_Corretamente()
+    {
+        // Arrange
+        GeneroFilmeIndexPageObject generoFilmeIndex = new(driver);
+
+        generoFilmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Comédia")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndex = new(driver);
+
+        // Act
+
+        FilmeFormPageObject filmeForm = filmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar();
+
+        filmeForm
+            .PreencherTitulo("Esposa de Mentirinha")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Comédia")
+            .ClickSubmit();
+
+        // Assert
+        Assert.IsTrue(filmeIndex.ContemFilme("Esposa de Mentirinha"));
+    }
+}
