286daa7383f812765297b7a815cac3eb17114171
gsvsantos <gustavocnsantos02@gmail.com>
2025-08-26 16:49:52 -0300

    feat: Implementa teste de interface, de exclusão do ModuloSessao.

--- ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTestes.cs
+++ ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTestes.cs
@@ -126,4 +126,57 @@ public class SessaoInterfaceTestes : TestFixture
         // Assert
         Assert.IsTrue(sessaoIndex.ContemSessao("Todo Mundo Tem a Irmã Gêmea Que Merece"));
     }
+
+    [TestMethod]
+    public void Deve_Excluir_Sessao_Corretamente()
+    {
+        // Arrange
+        GeneroFilmeIndexPageObject generoFilmeIndex = new(driver);
+        generoFilmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Comédia")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndex = new(driver);
+        filmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Esposa de Mentirinha")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Comédia")
+            .ClickSubmit();
+
+        SalaIndexPageObject salaIndex = new(driver);
+        salaIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherNumero(5)
+            .PreencherCapacidade(50)
+            .ClickSubmit();
+
+        SessaoIndexPageObject sessaoIndex = new(driver);
+        sessaoIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherInicio("2025-08-12T14:15")
+            .PreencherNumeroMaximoIngressos(6)
+            .SelecionarFilme("Esposa de Mentirinha")
+            .SelecionarSala(5)
+            .ClickSubmit();
+
+        // Act
+        SessaoFormPageObject sessaoForm = sessaoIndex
+            .IrPara(enderecoBase)
+            .ClickEncerrar()
+            .ClickSubmitEncerrar()
+            .ClickExcluir();
+
+        sessaoForm
+            .ClickSubmitExcluir("Esposa de Mentirinha");
+
+        // Assert
+        Assert.IsFalse(sessaoIndex.ContemSessao("Esposa de Mentirinha"));
+    }
 }
