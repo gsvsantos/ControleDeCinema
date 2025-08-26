30784e5f5c3aa79f571c7e3340ac15f6d29ca97e
gsvsantos <gustavocnsantos02@gmail.com>
2025-08-26 15:38:50 -0300

    feat: Implementa teste de interface, de edição do ModuloSessao.

--- ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTestes.cs
+++ ControleDeCinema.Testes.Interface/ModuloSessao/SessaoInterfaceTestes.cs
@@ -63,4 +63,67 @@ public class SessaoInterfaceTestes : TestFixture
         // Assert
         Assert.IsTrue(sessaoIndex.ContemSessao("Esposa de Mentirinha"));
     }
+
+    [TestMethod]
+    public void Deve_Editar_Sessao_Corretamente()
+    {
+        // Arrange
+        GeneroFilmeIndexPageObject generoFilmeIndex = new(driver);
+        generoFilmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherDescricao("Comédia")
+            .ClickSubmit();
+
+        FilmeIndexPageObject filmeIndex = new(driver);
+        filmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Esposa de Mentirinha")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Comédia")
+            .ClickSubmit();
+        filmeIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherTitulo("Todo Mundo Tem a Irmã Gêmea Que Merece")
+            .PreencherDuracao(117)
+            .MarcarLancamento()
+            .SelecionarGenero("Comédia")
+            .ClickSubmit();
+
+        SalaIndexPageObject salaIndex = new(driver);
+        salaIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherNumero(5)
+            .PreencherCapacidade(50)
+            .ClickSubmit();
+
+        SessaoIndexPageObject sessaoIndex = new(driver);
+        sessaoIndex
+            .IrPara(enderecoBase)
+            .ClickCadastrar()
+            .PreencherInicio("2025-08-12T14:15")
+            .PreencherNumeroMaximoIngressos(6)
+            .SelecionarFilme("Esposa de Mentirinha")
+            .SelecionarSala(5)
+            .ClickSubmit();
+
+        // Act
+        SessaoFormPageObject sessaoForm = sessaoIndex
+            .IrPara(enderecoBase)
+            .ClickEditar();
+
+        sessaoForm
+            .PreencherInicio("2025-08-22T14:30")
+            .PreencherNumeroMaximoIngressos(12)
+            .SelecionarFilme("Todo Mundo Tem a Irmã Gêmea Que Merece")
+            .SelecionarSala(5)
+            .ClickSubmit();
+
+        // Assert
+        Assert.IsTrue(sessaoIndex.ContemSessao("Todo Mundo Tem a Irmã Gêmea Que Merece"));
+    }
 }
